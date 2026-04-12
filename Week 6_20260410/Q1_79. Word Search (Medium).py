ff46f942665ff5d48fbd9efbf3be6f928038895a
Chao0219 <egwl.chao0219@outlook.com>
2026-04-12 15:34:35 +0800

    1/3

---
+++ Week 6_20260410/Q1_79. Word Search (Medium).py
@@ -0,0 +1,30 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        rows, cols = len(board), len(board[0])
+        def backtrack(r, c, index):
+            # if length of index equals to word, return true
+            if index == len(word): return True
+
+            # BC
+            if (r < 0 or r >= rows or c < 0 or c >= cols or board[r][c] != word[index]):
+                return False
+            
+            temp = board[r][c]
+            board[r][c] = "#"
+
+            # Searching for four direction
+            found = (backtrack(r + 1, c, index + 1) or
+                     backtrack(r - 1, c, index + 1) or
+                     backtrack(r, c + 1, index + 1) or
+                     backtrack(r, c - 1, index + 1))
+            
+            board[r][c] = temp
+            return found
+        # iteration for finding
+        for r in range(rows):
+            for c in range(cols):
+                if backtrack(r, c, 0):
+                    return True
+        
+        return False
+            
